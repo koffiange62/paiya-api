a04ffe3902ae3ce7225d8c85d76f8898fd35349f
koffiange62@gmail.com <koffiange62@gmail.com>
2025-11-30 22:49:51 -0500

    INITIALISER LE PROJET
    Creer le repo git
    Creer le projet dans github

---
+++ src/main/java/com/paiya/paiya_api/PaiyaApiApplication.java
@@ -0,0 +1,13 @@
+package com.paiya.paiya_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiyaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaiyaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paiya/paiya_api/PaiyaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paiya.paiya_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaiyaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
